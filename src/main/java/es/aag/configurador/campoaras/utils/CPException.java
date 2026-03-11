5d0e1ea6715e8b952e1f026bd4ee0fb05eb09043
DesarrolloIT-A3com <desarrolloit@a3com.es>
2026-03-11 10:43:02 +0100

    Se añade getter en la excepcion para no añadir métodos irrelevantes

--- src/main/java/es/aag/configurador/campoaras/utils/CPException.java
+++ src/main/java/es/aag/configurador/campoaras/utils/CPException.java
@@ -4,9 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class CPException extends Exception
 {
