78e44443233228dae4355553ea883b411a62704c
DesarrolloIT-A3com <desarrolloit@a3com.es>
2026-05-12 09:13:14 +0200

    Se actualizan los estados de los pedidos

--- src/main/java/es/aag/configurador/campoaras/utils/EstadoPedido.java
+++ src/main/java/es/aag/configurador/campoaras/utils/EstadoPedido.java
@@ -2,5 +2,5 @@ package es.aag.configurador.campoaras.utils;
 //TODO: Reemplazar todos los estados por CURSADO y NO_CURSADO
 public enum EstadoPedido 
 {
-	ACEPTADO,RECHAZADO, NO_CURSADO, CURSADO
+	NO_CURSADO, CURSADO
 }
