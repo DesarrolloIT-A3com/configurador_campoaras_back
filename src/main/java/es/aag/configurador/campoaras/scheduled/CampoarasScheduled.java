b8ec60a79307e29795bde01a8ff69ce17e281ad3
DesarrolloIT-A3com <desarrolloit@a3com.es>
2026-06-18 09:54:15 +0200

    Se incluye la eliminación de pedidos bak en el cron

--- src/main/java/es/aag/configurador/campoaras/scheduled/CampoarasScheduled.java
+++ src/main/java/es/aag/configurador/campoaras/scheduled/CampoarasScheduled.java
@@ -15,9 +15,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import es.aag.configurador.campoaras.entities.BulkProductosUsuario;
 import es.aag.configurador.campoaras.entities.Pedido;
+import es.aag.configurador.campoaras.entities.PedidoBackup;
+import es.aag.configurador.campoaras.entities.PedidoBackup;
 import es.aag.configurador.campoaras.entities.ProductoConfigurado;
 import es.aag.configurador.campoaras.entities.Usuario;
 import es.aag.configurador.campoaras.repositories.IBulkProductosUsuarioRepository;
+import es.aag.configurador.campoaras.repositories.IPedidoBackupRepository;
 import es.aag.configurador.campoaras.repositories.IPedidoRepository;
 import es.aag.configurador.campoaras.repositories.IProductoConfiguradoRepository;
 import es.aag.configurador.campoaras.repositories.IUsuarioRepository;
@@ -42,6 +45,9 @@ public class CampoarasScheduled
 	private IPedidoRepository pedidoRepo;
 	
 	@Autowired
+	private IPedidoBackupRepository pedidoBakRepo;
+	
+	@Autowired
 	private EncryptorService encryptor;
 	
 	
@@ -55,10 +61,13 @@ public class CampoarasScheduled
 		LocalDateTime now = LocalDateTime.now().minusDays(45);
 		log.info("[ADMIN] -- /schedule -- Comienzo del proceso de eliminación de configuraciones obsoletas");
 		List<BulkProductosUsuario> bulks = this.bulkRepo.findAll();
+		List<PedidoBackup> pedidosBacks = this.pedidoBakRepo.findAll();
 		List<Pedido> pedidos = this.pedidoRepo.findAll();
 		List<BulkProductosUsuario> toDelete = new LinkedList<BulkProductosUsuario>();
 		List<ProductoConfigurado> selecciones = new LinkedList<ProductoConfigurado>();
+		List<PedidoBackup> backDelete = new LinkedList<PedidoBackup>();
 		List<Usuario> affected = new LinkedList<Usuario>();
+		List<Usuario> affectedBack = new LinkedList<Usuario>();
 		
 		for(BulkProductosUsuario item:bulks)
 		{
@@ -73,7 +82,7 @@ public class CampoarasScheduled
 			}
 			else
 			{
-				log.info("[AVISO] Fecha en configuraciones nula");
+				log.info("[ADMIN] -- /schedule -- Fecha de la configuracion {} nula se procederá a borrado",this.encryptor.decrypt(item.getReferencia()));
 			}
 			
 			if(isOld)
@@ -117,6 +126,31 @@ public class CampoarasScheduled
 			}
 		}
 		
+		for(PedidoBackup pedido:pedidosBacks)
+		{
+			boolean isOld = true;
+			LocalDateTime other = pedido.getFecha();
+			
+			if(other!=null)
+			{
+				isOld = now.isAfter(other);
+			}
+			else
+			{
+				log.info("[ADMIN] -- /schedule -- Fecha de pedido back {} nula, se procederá a su borrado",this.encryptor.decrypt(pedido.getReferencia()));
+			}
+			
+			if(isOld)
+			{
+				Usuario usuario = pedido.getUsuarioPedidoBack();
+				usuario.removePedidoBak(pedido);
+				affectedBack.add(usuario);
+				backDelete.add(pedido);
+				log.info("[ADMIN] -- /scheduled -- Se han detectado {} copias de pedido obsoletas del usuario {}",backDelete.size(),usuario.getUSRToken());
+			}
+			
+		}
+		
 		if(affected.isEmpty() || toDelete.isEmpty() || selecciones.isEmpty())
 		{
 			log.info("[ADMIN] -- /schedule -- No se han encontrado configuraciones a borrar");
@@ -124,12 +158,26 @@ public class CampoarasScheduled
 		else
 		{
 			log.info("[ADMIN] -- /schedule -- Se han borrado {} configuraciones obsoletas",selecciones.size());
+			this.userRepo.saveAll(affected);
+			this.userRepo.flush();
 			this.productoRepo.deleteAll(selecciones);
 			this.productoRepo.flush();
 			this.bulkRepo.deleteAll(toDelete);
 			this.bulkRepo.flush();
-			this.userRepo.saveAll(affected);
+			
+		}
+		
+		if(backDelete.isEmpty())
+		{
+			log.info("[ADMIN] -- /schedule -- No se han encontrado copias de pedido a borrar");
+		}
+		else
+		{
+			log.info("[ADMIN] -- /schedule -- Se han borrado {} copias de pedido obsoletos",backDelete.size());
+			this.userRepo.saveAll(affectedBack);
 			this.userRepo.flush();
+			this.pedidoBakRepo.deleteAll(backDelete);
+			this.pedidoBakRepo.flush();
 		}
 	}
 	
